dc4e756391cc293733f1e640a982862783b7b652
Sweta Mohapatra <sweta.mohapatra19@gmail.com>
2024-04-21 21:33:57 +0530

    Initial Commit

---
+++ frontend/build/precache-manifest.dd6ff7cf0c60aab89e9c418a16a5987e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "688d45fccc7b55b4db00a0747f461027",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c4662c0b99a081b58a3",
+    "url": "/static/css/main.c3ae1698.chunk.css"
+  },
+  {
+    "revision": "87646761102acd1c6a32",
+    "url": "/static/js/2.cb94bc6a.chunk.js"
+  },
+  {
+    "revision": "6c4662c0b99a081b58a3",
+    "url": "/static/js/main.ae7cdf95.chunk.js"
+  },
+  {
+    "revision": "51810371bf2cf35f1e22",
+    "url": "/static/js/runtime-main.85498786.js"
+  }
+]);
